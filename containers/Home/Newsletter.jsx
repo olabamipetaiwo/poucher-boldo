5c34cd458333b2a44b1baa28c9be295ed80e7c67
olabamipe.taiwo <olabamipe.taiwo@mypatricia.co>
2023-07-27 17:36:40 +0100

    dev::Feature mobile

--- containers/Home/Newsletter.jsx
+++ containers/Home/Newsletter.jsx
@@ -2,16 +2,18 @@
 
 const Newsletter = () => {
   return (
-    <section className="bg-dark-blue mx-8 sm:mx-16 tab:mx-20 lg:mx-24 py-24 flex flex-col items-center text-white  rounded-xl">
-      <p className="text-center w-3/4 header-text font-body mb-16">
+    <section className="bg-dark-blue mx-0 tab:mx-20 lg:mx-24 py-24 flex flex-col items-center text-white lg:rounded-xl">
+      <p className="text-center lg:w-3/4 header-text font-body mb-16">
         An enterprise template to ramp up your company website
       </p>
 
       <section className="form w-[80%] lg:w-1/2 mx-auto">
-        <div className="form-group basis-[60%]">
+        <div className="form-group basis-full lg:basis-[60%] mb-4 lg:mb-0">
           <input name="email" type="email" placeholder="Your email address" />
         </div>
-        <button className="btn btn-secondary">Start now</button>
+        <div className="basis-full lg:basis-[35%] ">
+          <button className="btn btn-secondary !w-full">Start now</button>
+        </div>
       </section>
     </section>
   );
